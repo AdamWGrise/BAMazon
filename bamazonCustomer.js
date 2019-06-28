050dd7c55304aec52344c4140f4ca6364923041d
Adam Grise <adamwgrise@gmail.com>
2019-06-28 01:07:57 -0500

    Changed to a menu-based interaction. Challenge #1 requirements completed.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var validItems = [];
+var fails = 0;
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -13,29 +14,65 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("Connected as ID " + connection.threadId);
-    readAllData();
+    if (process.argv[2] === 'restockItems') {
+        restockItems();
+    } else {
+        readAllData();
+    }
 });
+
+function restockItems() {
+    connection.query("UPDATE products SET stock_quantity = 500;",
+        function (err, res) {
+            if (err) throw err;
+            console.log("\nStore restocked.\n");
+            connection.end();
+            process.exit();
+        });
+};
 
 function readAllData() {
     connection.query("SELECT * FROM products;",
         function (err, res) {
             if (err) throw err;
+            validItems = [];
             for (i = 0; i < res.length; i++) {
-                console.log('\nItem ' + res[i].item_id + ': ' + res[i].product_name);
-                console.log('  in ' + res[i].department_name);
-                console.log('  $' + res[i].price + '  |  ' + res[i].stock_quantity + ' in stock');
+                console.log('\nItem ' + res[i].item_id + ': "' + res[i].product_name + '", in ' + res[i].department_name);
+                console.log('  $' + res[i].price + ' each  |  ' + res[i].stock_quantity + ' in stock');
                 validItems.push(res[i].item_id);
             };
             console.log('\n');
-            askCustomer();
+            menu();
         });
 };
+
+function menu() {
+    if(fails > 4) {
+        console.log("\n\n\n\n\n\n\n\n===================================\nYeah... you know what? You've had so many issues just reading and typing coherently that I'm pretty sure you should step away from the computer and stop buying stuff. Come back tomorrow now, y'hear?");
+        connection.end();
+        process.exit();
+    }
+    inquirer.prompt({
+        type: "list",
+        name: "selection",
+        message: "\nWhat would you like to do?",
+        choices: ["Show me the catalog again.", "Buy somethin'!", "I'm done. Time to go."]
+    }).then(function (answers) {
+        if (answers.selection === "Show me the catalog again.") {
+            readAllData();
+        } else if (answers.selection === "Buy somethin'!") {
+            askCustomer();
+        } else {
+            endShoppingDay();
+        }
+    });
+}
 
 function askCustomer() {
     inquirer.prompt([{
             type: "input",
             name: "productNo",
-            message: "\nEnter the Item number of the item you'd like to purchase:\n "
+            message: "\nEnter the Item number of the item you'd like to purchase.\n "
         },
         {
             type: "input",
@@ -43,32 +80,21 @@ function askCustomer() {
             message: "\nHow many would you like to buy?\n"
         }
     ]).then(function (answers) {
-        if(answers.productNo != parseInt(answers.productNo,10)) {
+        if (answers.productNo != parseInt(answers.productNo, 10)) {
             console.log('Make sure you enter a NUMBER for the Item number. You can find the Item number in the listing above!');
-            tryAgain();
-        } else if (answers.qty != parseInt(answers.qty,10)) {
+            fails++;
+            menu();
+        } else if (answers.qty != parseInt(answers.qty, 10)) {
             console.log('Make sure you enter a NUMBER for the quantity of items you want to purchase!');
-            tryAgain();
-        } else if (!validItems.includes(parseInt(answers.productNo,10))) {
+            fails++;
+            menu();
+        } else if (!validItems.includes(parseInt(answers.productNo, 10))) {
             console.log('You need to pick an Item number from an item in the listings!');
-            tryAgain();
+            fails++;
+            menu();
         } else {
             buyItem(answers.productNo, answers.qty);
         };
-    });
-};
-
-function tryAgain() {
-    inquirer.prompt([{
-        type: "confirm",
-        name: "tryAgain",
-        message: "\nPick something else to buy?\n"
-    }]).then(function (answers) {
-        if (answers.tryAgain) {
-            askCustomer();
-        } else {
-            endShoppingDay();
-        }
     });
 };
 
@@ -80,6 +106,9 @@ function buyItem(productNo, qty) {
             if (err) throw err;
             if (res[0].stock_quantity === 0) {
                 outOfStock(res[0]);
+                console.log("\nI'm sorry; '" + res[0].product_name + "' is completely out of stock. Feel free to purchase something else or wait until we've restocked!\n");
+                fails++;
+                menu();
             } else if (qty > res[0].stock_quantity) {
                 buyOut(qty, res[0]);
             } else {
@@ -88,22 +117,8 @@ function buyItem(productNo, qty) {
         });
 };
 
-// NEEDS TO BE TESTED
-function outOfStock(item) {
-    inquirer.prompt([{
-        type: "confirm",
-        name: "shopAgain",
-        message: "\nI'm sorry; '" + item.product_name + "' is completely out of stock. Would you like to purchase something else?\n"
-    }]).then(function (answers) {
-        if (answers.shopAgain) {
-            askCustomer();
-        } else {
-            endShoppingDay();
-        }
-    });
-};
-
 function buyOut(qty, item) {
+    fails++;
     inquirer.prompt([{
         type: "confirm",
         name: "buyOut",
@@ -112,31 +127,38 @@ function buyOut(qty, item) {
         if (answers.buyOut) {
             completePurchase(item.stock_quantity, item);
         } else {
-            tryAgain();
+            menu();
         }
     });
 }
 
 function completePurchase(qty, item) {
-    ///////// TEMP ////////////
-    console.log("\n\n\nPurchase: " + item.product_name + '\nAmount purchased: ' + qty)
-    // Do stuff to the DB here, refine purchase message.
-    ///////////////////////////
-    inquirer.prompt([{
-        type: "confirm",
-        name: "shopAgain",
-        message: "\nAnything else you'd like to buy today?\n"
-    }]).then(function (answers) {
-        if (answers.shopAgain) {
-            askCustomer();
-        } else {
-            endShoppingDay();
-        }
-    });
+    connection.query("UPDATE products SET ? WHERE ?", [{
+                stock_quantity: item.stock_quantity - qty
+            },
+            {
+                item_id: item.item_id
+            }
+        ],
+        function (err, res) {
+            if (err) throw err;
+            var subtotal = Math.floor((qty*item.price)*100)/100;
+            var tax = Math.floor((qty*item.price*0.07125)*100)/100;
+            var total = Math.floor((qty*item.price*1.07125)*100)/100;;
+            console.log("\n\n\nThanks for your purchase! Here's your receipt.");
+            console.log("\n\n###############################\n\nItem: " + item.product_name +
+            '\nQuantity: ' + qty +
+            '\n\n-------------------------------' +
+            '\nSubtotal: $' + subtotal +
+            '\nTax: $' + tax +
+            '\n\n===============================' +
+            '\nTOTAL: $' + total + '\n\n###############################\n\n');
+            menu();
+        });
 };
 
 function endShoppingDay() {
     connection.end();
     console.log("\n\nThanks for stopping by! Have a lovely day.\n\n");
     process.exit();
-}
+};
